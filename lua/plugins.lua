5e6f6430d6bf4a30e8535add2dff58d618913341
gsxhnd <gsxhnd@gmail.com>
2022-05-04 22:09:55 +0800

    rm lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,20 +4,15 @@ return require('packer').startup(function()
     -- theme
     use 'shaunsingh/nord.nvim'
     -- layout plugin
-    use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
-    use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons'}
-    use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
+    use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' }
+    use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
+    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
     use 'simrat39/symbols-outline.nvim'
-    use {'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
-    use {'nvim-treesitter/nvim-treesitter',run = ':TSUpdate'}
+    use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use {
         "SmiteshP/nvim-gps",
         requires = "nvim-treesitter/nvim-treesitter"
-    }
-    -- lsp plugin
-    use {
-        'neovim/nvim-lspconfig',
-        'williamboman/nvim-lsp-installer',
     }
     -- key mapping
     use 'folke/which-key.nvim'
@@ -25,22 +20,26 @@ return require('packer').startup(function()
     use {
         'lewis6991/gitsigns.nvim',
         requires = {
-          'nvim-lua/plenary.nvim'
+            'nvim-lua/plenary.nvim'
         }
     }
     use "Pocco81/AutoSave.nvim"
-    -- nvim-cmp vsnip
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/vim-vsnip'
-    -- use 'rafamadriz/friendly-snippets'
-    use 'onsails/lspkind-nvim'
- 	use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
-    -- golang
-    use 'fatih/vim-go'
-end)
 
+    -- lsp plugin
+    -- use {
+    --     'neovim/nvim-lspconfig',
+    --     'williamboman/nvim-lsp-installer',
+    -- }
+    -- nvim-cmp vsnip
+    -- use 'hrsh7th/cmp-nvim-lsp'
+    -- use 'hrsh7th/cmp-buffer'
+    -- use 'hrsh7th/cmp-path'
+    -- use 'hrsh7th/cmp-cmdline'
+    -- use 'hrsh7th/nvim-cmp'
+    -- use 'hrsh7th/cmp-vsnip'
+    -- use 'hrsh7th/vim-vsnip'
+    -- use 'onsails/lspkind-nvim'
+    -- use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
+    -- golang
+    -- use 'fatih/vim-go'
+end)
